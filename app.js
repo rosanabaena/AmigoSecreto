1774b9c57422f78172a6aaf1bca25e302e7c8b07
rosana <rosanabaena@gmail.com>
2025-02-12 09:47:56 -0300

    Adicionado funcao sortear amigos

--- app.js
+++ app.js
@@ -2,24 +2,23 @@
 
 //array para armazenar os nomes
 let amigos = [];
+let sorteado = 0;
 
-/*
-//mostra na tela o texto de acordo com a tag e texto informados
-function carregaLista(tag,texto){
-    let campo = document.getElementById(tag);
-    campo.innerHTML = texto;
+function atualizaElemento(tag,texto){
+    let list = document.getElementById(tag);
+    list.innerHTML = texto;
+ }
+
+//limpar o campo do nome depois de incluido na lista
+function limpaCampo(){
+    campo = document.getElementById('amigo');
+    campo.value = '';
 }
 
-//listar todos amigos ('ul') que estao no array
-function listaAmigos(){
-    carregaLista('listaAmigos',amigos);
-}
-*/
-//percorre o array amigos e adicione cada nome como um elemento <li> 
 function list(){
     // Seleciona o elemento <ul> onde os <li> serão adicionados
     let list = document.getElementById('listaAmigos');
-    list.innerHTML = "";
+    list.innerHTML = '';
     let li = document.createElement("li");
     
     // Percorre o array de amigos e adiciona cada nome como um elemento<li>
@@ -28,8 +27,7 @@ function list(){
         if (amigos[i] !== undefined && amigos[i] !== null) {    
             list.innerHTML += `<li>${amigos[i]}</li>`;
             console.log(amigos[i]);    
-        }
-        
+        } 
     }
 }
 
@@ -41,7 +39,7 @@ function validaNomeDigitado(nomeDigitado){
     //valida nome duplicado
     if (amigos.includes(nome)){
         alert(`O nome ${nome} já está na lista`);
-        limpaNome();
+        limpaCampo();
         return;
     }
 
@@ -58,21 +56,12 @@ function validaNomeDigitado(nomeDigitado){
     }
     //adicionar o nome na lista de amigos
     amigos.push(nome);
-    
-}
-
-//limpar o campo do nome depois de incluido na lista
-function limpaNome(){
-    campo = document.getElementById('amigo');
-    campo.value = '';
 }
 
 //adicionar nome na lista
 function adicionarAmigo(){
     let nomeDigitado = document.getElementById('amigo').value; 
-    validaNomeDigitado(nomeDigitado);
-    
-    /*
+  
     //identificar os nomes digitados separados por virgulas e inclui separados na lista
     if(nomeDigitado.includes(',')){
         let nomesDigitados = nomeDigitado.split(',');
@@ -83,31 +72,57 @@ function adicionarAmigo(){
     }else {
         validaNomeDigitado(nomeDigitado);
     }
-    */
-   
-    //listar nomes do array como elemento li
+       
+    //listar nomes do array no elemento 'ul' como 'li'
     list();
   
     //limpar o campo
-    limpaNome();
+    limpaCampo();
         
+    //nao exibe o elemento resultado quando está adicionando amigos
+    atualizaElemento('resultado',''); 
 }
 
+//sortear amigo / exibir resultado / limpar lista
+//sortearAmigo()
+ function sortearAmigo(){
+    
+    //Validar que há amigos disponíveis: Antes de sortear, verificar se o array amigos não está vazio.  
+    let quantidadeAmigos = amigos.length;
+    console.log(`quantidade de amigos: ${quantidadeAmigos}`);
+
+    //if (sorteado == 0){
+        if (quantidadeAmigos >= 2) {
+            //Gerar um índice aleatório: Usar Math.random() e Math.floor() para selecionar um índice aleatório do array.
+            let indiceSorteado = parseInt(Math.random() * (Math.floor(quantidadeAmigos)) )  ;
+            console.log (`Indice sorteado: ${indiceSorteado}`);
+            
+            //Obter o nome sorteado: Utilizar o índice aleatório para acessar o nome correspondente no array.
+            let amigoSorteado = amigos[indiceSorteado];
+            
+            //Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById() e innerHTML para exibir o amigo sorteado.
+            let resultado = document.getElementById('resultado');
+            resultado.innerHTML = (amigoSorteado);
+            
+            sorteado = 1;
+
+            atualizaElemento('listaAmigos','');
+        
+        }else{
+            alert('Necessário ao menos 2 amigos na lista para sortear!');
+        }
+    }
+//}
 
 
+//Reiniciar Amigo Secreto
+function reiniciarJogo(){
+    atualizaElemento('amigo','');
+    atualizaElemento('resultado','');
+    atualizaElemento('listaAmigos','');
+    amigos = [];
+    sorteado = 0;
 
+}
 
-
-
-
-
-
-
-
-
-//como exibir a lista de acordo com a digitaçao do nome?
-
-//resultado sorteado
-//exibirNaTela("resultado","Lorena");
-
-
+//verificar os nome que ja foram sorteados
